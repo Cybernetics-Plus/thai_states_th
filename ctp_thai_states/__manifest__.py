ae1cbbc077c467fe245705a0d2a6521f5ad4e971
BOMEN <bomen21x@gmail.com>
2022-12-31 01:02:01 +0700

    Start

---
+++ ctp_thai_states/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    Cybernetics Plus Co., Ltd.
+#    Thai Localization to include the Thailand States.
+#
+#    Copyright (C) 2021-TODAY Cybernetics Plus Technologies (<https://www.cybernetics.plus>).
+#    Author: Cybernetics Plus Techno Solutions (<https://www.cybernetics.plus>)
+#
+###################################################################################
+
+{
+    "name": "Thailand States (Eng Language)",
+    "version": "16.0.1.0.1",
+    "summary": """ 
+            Thai Localization to include the Thailand States.
+            """,
+    "description": """ 
+            Thai Localization to include the Thailand States.
+            """,
+    "author": "Cybernetics Plus Co., Ltd.",
+    "website": "https://www.cybernetics.plus",
+    "live_test_url": "https://www.cybernetics.plus",
+    'images': ['static/description/banner.gif'],
+    "category": "Cybernetics Plus",
+    "license": "AGPL-3",
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+    "contributors": [
+        "C+ Developer <dev@cybernetics.plus>",
+    ],
+    'data': [
+        'data/res.country.state.csv',        
+    ],
+}
